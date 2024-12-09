d4fe3a2c5d26d8d24e0f92d78f64ba00a9b26e2d
FrankHuarcaya <u201624288@upc.edu.pe>
2024-12-09 10:45:04 -0500

    create microservices exam

---
+++ microservices-exams/src/main/java/com/example/microservices/app/exams/MicroservicesExamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservices.app.exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesExamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesExamsApplication.class, args);
+	}
+
+}
---
+++ microservices-exams/src/test/java/com/example/microservices/app/exams/MicroservicesExamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices.app.exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesExamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
